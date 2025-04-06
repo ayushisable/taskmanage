e38fd797bb713d1d00fb8e7e3f569b35e68f1a92
ayushisable <subsable11@gmail.com>
2025-04-06 14:40:54 +0530

    username

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -3,6 +3,8 @@ import { setLocalStorage } from '../../utils/localStorage'
 
 const Header = (props) => {
 
+const name =  props.data.firstname
+
   // const [username, setUsername] = useState('')
 
   // if(!data){
@@ -20,7 +22,7 @@ const Header = (props) => {
   
   return (
     <div className='flex items-end justify-between'>
-        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>username 👋</span></h1>
+        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{name} 👋</span></h1>
         <button onClick={logOutUser} className='bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
     </div>
   )
